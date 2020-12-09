2d32470371448e973f0c83b41a59d88c3001b058
incxption <theincxption@gmail.com>
2020-12-09 22:23:46 +0100

    :heavy_plus_sign: Add kotlin-argparser depdendency and include it in the final JAR file

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,21 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+}
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
 tasks {
     getByName<Jar>("jar") {
+        manifest {
+            attributes["Main-Class"] = "net.dragonfly.obfuscation.wrapper.SpecialSourceWrapper"
+        }
+
+        from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
         archiveName = "obfuscation-api.jar"
     }
 }
